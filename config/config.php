af889e467628ad72a2c9f46238b51696eff880bc
Jacob Bednarz <jacob.bednarz@gmail.com>
2012-03-29 01:56:02 +1100

    Initial roll out

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// File check - pretty arbitary
+define('FILE_CHECK', 'mf7gf3jh');
+
+// Name of directory where markdown posts are stored
+define('POSTS_DIR', 'posts_source');
+
+// Admin email address for debugging and notifications
+define('ADMIN_EMAIL', 'jacob.bednarz@gmail.com');
---
+++ view_post.php
@@ -0,0 +1,30 @@
+<?php
+
+// Include the class and simple configuration file
+include_once 'markdown.php';
+include_once 'config/config.php';
+
+$requested_post = htmlspecialchars(trim(stripslashes($_GET['name'])));
+$posts_url = POSTS_DIR . DIRECTORY_SEPARATOR . $requested_post . '.md';
+
+if (file_exists($posts_url) && preg_match('/[^a-z_\-0-9]/i', $posts_url))
+{
+	$output = Markdown(file_get_contents(POSTS_DIR . DIRECTORY_SEPARATOR . $requested_post . '.md'));
+
+	include_once 'layouts/header.html';
+	echo $output;
+	include_once 'layouts/footer.html';
+}
+else
+{
+	/**
+	 * This is about as simple as it gets. I would be recommending
+	 * that you change this to a redirect, throw an error, or do
+	 * something way cooler than outputting 'Post does not exist'.
+	 * 
+	 * I may choose to update this with something like a unicorn
+	 * or a dragon breathing fire but to keep it flexible, I will
+	 * leave it be for now.
+	 */
+	echo 'Post does not exist';
+}
