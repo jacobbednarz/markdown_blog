fd2e6b4ae03dd0264c29cb56046366b0b91a573a
Jacob Bednarz <jacob.bednarz@gmail.com>
2012-04-08 15:00:29 +1000

    Added ability to view all posts and links to posts from blog index page

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+// Include global config
+include_once 'config/config.php';
+
+// Display the header files
+include_once 'layouts/header.html';
+echo '<h1>The blog archive</h1>';
+
+function format_blog_title($title)
+{
+    return ucfirst(str_replace(array('posts_source/', '.md', '-'), array('', '', ' '), $title));
+}
+
+function path_to_post($title)
+{
+    return str_replace(array(POSTS_DIR, '.md'), array(''), $title);
+}
+
+$directory = POSTS_DIR . '/';
+if (glob($directory . '*.md') != FALSE)
+{
+    $file_count = count(glob($directory . '*.md'));
+    $files = glob($directory . '*.md');
+    foreach ($files as $file)
+    {
+        echo '<p><a href="posts' . path_to_post($file) . '">' . format_blog_title($file) . '</a></p>';
+    }
+}
+
+include_once 'layouts/footer.html';
+
+?>
